695c1a444a80fe12791a96f5f9096040736baad2
Pavel Škoda <p@v3l.cz>
2024-01-17 17:17:13 +0100

    Add swipe prototype (Need to be refactored :))

--- Scenes/UI/Card.gd
+++ Scenes/UI/Card.gd
@@ -4,6 +4,8 @@ var cardDatabase = preload("res://Scripts/Objects/CardDatabase.gd")
 
 var cardName
 var cardStats: Stats
+
+var viewport_width
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,3 +24,29 @@ func _ready():
 	cardTextControl.text = cardInfo[1]
 	
 	cardStats = Stats.new(cardInfo[2][0], cardInfo[2][1], cardInfo[2][2], cardInfo[2][3])
+	
+	# swipe support
+	viewport_width = get_viewport_rect().size.x
+	
+
+var swipe_start_pos
+var swipe_sensitivity = 0.7
+var max_rotation = PI/20
+var swipe_len = 0
+
+func _input(ev):
+	if ev is InputEventScreenTouch:
+		if ev.pressed:
+			swipe_start_pos = ev.position.x
+		else:
+			rotation = 0
+		
+	if ev is InputEventScreenDrag:
+		swipe_len = (ev.position.x - swipe_start_pos) * swipe_sensitivity / viewport_width
+		swipe_len = clamp(swipe_len, -max_rotation, max_rotation)
+		rotation = swipe_len
+		
+
+
+
+	
